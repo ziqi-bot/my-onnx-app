c08dd4c0ae5dc21daa36bdd5ea327798704352b7
peter <ziqidou116@gmail.com>
2024-06-27 12:01:08 -0400

    Fix latest results parsing and display

--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -84,69 +84,61 @@
 
 
 
-
-
-
 import React from 'react';
 import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 
+interface Result {
+  id: number;
+  pedestrian: number;
+  biker: number;
+  skater: number;
+  cart: number;
+  car: number;
+  bus: number;
+  created_at: string;
+}
+
 interface SearchResultsProps {
-  searchResults: any[];
+  searchResults: Result[];
   mode: 'latest' | 'all';
 }
 
 const classNames = ["pedestrian", "biker", "skater", "cart", "car", "bus"];
 
 const SearchResults: React.FC<SearchResultsProps> = ({ searchResults, mode }) => {
-  if (mode === 'latest') {
-    const result = searchResults[0];
-    if (result) {
-      return (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Class</TableCell>
-              <TableCell>Details</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {classNames.map((className, index) => (
-              <TableRow key={index}>
-                <TableCell>{className}</TableCell>
-                <TableCell>{result[className] || 0}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      );
-    }
-    return <div>No results found</div>;
-  } else {
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>File</TableCell>
-            <TableCell>Details</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {searchResults.map((result, index) => (
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>{mode === 'latest' ? 'Class' : 'File'}</TableCell>
+          <TableCell>Details</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {mode === 'latest' ? (
+          classNames.map((className, index) => (
             <TableRow key={index}>
-              <TableCell>{result.filePath || result}</TableCell>
+              <TableCell>{className}</TableCell>
+              <TableCell>{searchResults.length > 0 ? (searchResults[0] as any)[className] : 0}</TableCell>
+            </TableRow>
+          ))
+        ) : (
+          searchResults.map((result, index) => (
+            <TableRow key={index}>
+              <TableCell>{result.id}</TableCell>
               <TableCell>
-                {classNames.map((className, classIndex) => (
+                {classNames.map((className) => (
                   <div key={className}>
-                    {className}: {result.content && result.content[classIndex] !== undefined ? result.content[classIndex] : 0}
+                    {className}: {(result as any)[className] !== undefined ? (result as any)[className] : 0}
                   </div>
                 ))}
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  }
+          ))
+        )}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default SearchResults;
